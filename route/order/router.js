4c6dd3ad024c18984416e36edb0a9ebda9aa5abc
NoHomey <ivo.stratev.tues@gmail.com>
2019-09-05 06:37:37 +0300

    adding route

--- route/order/router.js
+++ route/order/router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const authUser = require('./../../middleware/authUser');
 const makeOrder = require('./makeOrder');
+const fetchOrderData = require('./fetchOrderData');
 
 const router = express.Router();
 
 router.use(authUser);   
 
 router.post('/', makeOrder);
+router.get('/:id', fetchOrderData);
 
 module.exports = router;
